8cba35ca306fc5e4be4cd025520959ada6a3b785
Abhishek <abhishekk19@gmail.com>
2020-05-16 14:19:52 +0530

    footer updated.

--- src/components/Footer/footer.component.tsx
+++ src/components/Footer/footer.component.tsx
@@ -7,10 +7,10 @@ interface FooterProps { }
 const Footer: React.FC<FooterProps> = () => {
   return (
     <footer>
-      {'</> with '}
-      <span className='heart'>&hearts;</span>
-      {' and '}
-      <span className='react-logo'>&#x269B;</span>
+      <small>
+        {'Artwork and Code by'}&nbsp;
+        <a href='https://abhishek-kdm.github.io' target='_blank'>abhishek-kdm</a>
+      </small>
     </footer>
   )
 }
