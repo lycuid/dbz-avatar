ec8fee25dc13751a219d0057caf528841e820a67
Abhishek <abhishekk19@gmail.com>
2020-05-24 13:23:32 +0530

    favicon added, unnecessary image removed, background iaged updated.

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: package.name,
+        short_name: `dbz-personas`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-typescript`,
